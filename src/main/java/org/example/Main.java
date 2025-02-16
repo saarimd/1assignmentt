e62981846a0a1773f6fde01f6277d38d0b12bb7a
Saida <saida.sagintaeva30@gmail.com>
2025-02-16 17:34:36 +0500

    created project  with Tom and Jerry classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         Tom tom = context.getBean(Tom.class);
-        Jerry
+        Jerry jerry = context.getBean(Jerry.class);
+
+        System.out.println(tom.getName() + " is chasing " + jerry.getName());
     }
 }
